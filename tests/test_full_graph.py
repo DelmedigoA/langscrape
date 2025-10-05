02213e45a0ed3e303e41eaa2d69fa3e33403d981
Asaf Delmedigo <delmedigo@Asafs-Mac.local>
2025-10-05 12:32:42 +0300

    added api_keys.env path to config. wrapped test llm extraction inside a loop for multipile urls.

--- tests/test_full_graph.py
+++ tests/test_full_graph.py
@@ -1,44 +1,58 @@
-import asyncio
-from langscrape import fetch_html_patchright, final_print
 from langscrape.agent.graph import get_graph
-from langscrape.html.utils import clean_html_for_extraction3
 from langscrape.agent.tools import make_store_xpath, make_store_value
 from langscrape.utils import load_config, initialize_global_state, get_extractor, get_summarizer
-import json
 from dotenv import load_dotenv
-
-
 import os
 
-def beep():
-    os.system('afplay /System/Library/Sounds/Funk.aiff')  # or "Glass.aiff", "Submarine.aiff", etc.
+def start_sound():
+    os.system('afplay /System/Library/Sounds/Blow.aiff')
 
-def test_llm_extraction():
-    load_dotenv("api_keys.env")
+def end_sound():
+    os.system('afplay /System/Library/Sounds/Bottle.aiff')
+
+def add_sound(func):
+    def wrapper(*args, **kwargs):
+        start_sound()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_sound()
+    return wrapper
+
+@add_sound
+def test_llm_extraction(url: str):
     config = load_config()
-
+    load_dotenv(config["api_keys"])
     global_state = initialize_global_state(config)
-
     store_xpath = make_store_xpath(global_state)
     store_field_value = make_store_value(global_state)
     tools = [store_xpath, store_field_value]
     graph = get_graph(tools=tools)
-
     extractor = get_extractor(config)
     extractor_with_tools = extractor.bind_tools(tools, parallel_tool_calls=config["extractor"]["allow_parallel_tool_calls"])
     summarizer = get_summarizer(config)
-
     initial_state = {
         "messages": [],
-        "url": "https://www.middleeasteye.net/news/israel-soldier-boasted-running-over-dead-palestinian-man-tank",
+        "url": url,
         "global_state": global_state,  
         "extractor": extractor_with_tools,
         "summarizer": summarizer,
         "iterations": 0
     }
+    response = graph.invoke(initial_state)
+    return response
 
-    graph.invoke(initial_state)
 if __name__ == "__main__":
-    beep()  
-    test_llm_extraction()
-    beep()
+    urls = [
+        'https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(24)01169-3/fulltext',
+        'https://www.dropsitenews.com/p/how-gaza-health-ministry-counts-dead',
+        'https://theconversation.com/what-exactly-caused-the-explosion-at-a-hospital-in-gaza-without-an-independent-credible-investigation-it-will-be-hard-for-everyone-to-agree-216242'
+    ]
+    for idx, url in enumerate(urls):
+        print(f"[{idx+1} / {len(urls)}]")
+        print(f"working on {url.split('/')[-1]} from {url.split('/')[0]}...")
+        try:
+            test_llm_extraction(url)
+        except:
+            print("failed with {url}")
+
